edcb65f60d4c4148ccb1307796ae421af798dd26
Dwiky Damara <damaradwiky6@gmail.com>
2023-03-18 12:44:34 +0700

    adding top 5 popular news from detik.com package

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 This is main module
 """
 from EathquakeDetection import data_extraction, data_displaying
+from PopularNews import data_extraction1,data_displaying1
+
 
 if __name__ == "__main__":
-    print("Main Application")
+    print("Earthquake Detection Dahsboard!")
     result = data_extraction()
     data_displaying(result)
-    
+
+
+    print("\nPopular News Dashboard!")
+    result = data_extraction1()
+    data_displaying1(result)
